edddbb63545e171ebfe667ed8b59c088fd8851e7
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-08 23:09:09 +0200

    java comments added

--- app/src/main/java/pl/piotrdawidziuk/brightnessdemo/MainActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/brightnessdemo/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //NO IDEA WHAT TO PUT HERE TO BE FAIR 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
 }
